2c7f42e2921592bd10d9e4470e8c754139b5cdd7
Vladimir Paredes <vladimirparedesa@gmail.com>
2024-11-22 22:14:11 -0500

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+	"github.com/alexedwards/scs/v2"
+	`github.com/vparedes98/bookings/pkg/config`
+	"github.com/vparedes98/bookings/pkg/handlers"
+	"github.com/vparedes98/bookings/pkg/render"
+)
+
+const portNumber = ":8081"
+var app config.AppConfig
+var session *scs.SessionManager
+
+
+func main() {
+	
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	render.NewTemplates(&app)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+
+
+srv := &http.Server {
+	Addr: 		portNumber,
+	Handler: 	routes(&app),
+}
+
+err = srv.ListenAndServe()
+log.Fatal(err)
+
+}
+
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vparedes98/bookings
+
+go 1.23.3
+
+require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
